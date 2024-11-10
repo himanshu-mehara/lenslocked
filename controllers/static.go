096c34ff30f094e66af445c615d32b2c10c9d11a
Himanshu <mehra.himanshu1708@gmail.com>
2024-11-10 21:35:47 +0530

    decouple-with-interfaces

--- controllers/static.go
+++ controllers/static.go
@@ -3,36 +3,34 @@ package controllers
 import (
 	"html/template"
 	"net/http"
-	"webdev/views"
 )
 
-func StaticHandler(tpl views.Template) http.HandlerFunc {
+func StaticHandler(tpl Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
 	}
 }
 
-
-func FAQ(tpl views.Template) http.HandlerFunc {
-	questions := []struct{
-		Question string 
-		Answer template.HTML
+func FAQ(tpl Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
 	}{
 		{
-			Question : "is there are any free version",
-			Answer: "yes! we are offering a free trial for 30 days",
+			Question: "is there are any free version",
+			Answer:   "yes! we are offering a free trial for 30 days",
 		},
 		{
 			Question: "what are your support hours ?",
-			Answer: "we have support staff answering emails",
+			Answer:   "we have support staff answering emails",
 		},
 		{
-			Question : "how do i contact support",
-			Answer : "email us at ffdsaf;ds",
+			Question: "how do i contact support",
+			Answer:   "email us at ffdsaf;ds",
 		},
 		{
-			Question : "Where is your office located?",
-			Answer : "our entire team is remote !",
+			Question: "Where is your office located?",
+			Answer:   "our entire team is remote !",
 		},
 	}
 	return func(w http.ResponseWriter, r *http.Request) {
--- controllers/users.go
+++ controllers/users.go
@@ -2,15 +2,14 @@ package controllers
 
 import (
 	"net/http"
-	"webdev/views"
 )
 
 type Users struct {
 	Templates struct {
-		New views.Template
+		New Template
 	}
 }
 
 func (u Users) New(w http.ResponseWriter, r *http.Request) {
-	u.Templates.New.Execute(w,nil)
+	u.Templates.New.Execute(w, nil)
 }
