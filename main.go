037db2b1496c27bd55749b62bb65b2ac7fd7c658
Himanshu <mehra.himanshu1708@gmail.com>
2024-11-10 16:57:56 +0530

    signup using user controller

--- main.go
+++ main.go
@@ -39,24 +39,27 @@ func main() {
 	// }
 	fs := os.DirFS("templates")
 	r.Get("/", controllers.StaticHandler(
-		views.Must(views.ParseFS(fs,"home.gohtml","tailwind.gohtml"))))
+		views.Must(views.ParseFS(fs, "home.gohtml", "tailwind.gohtml"))))
 
 	// tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
 	// if err != nil {
 	// 	panic(err)
 	// }
 	r.Get("/contact", controllers.StaticHandler(
-		views.Must(views.ParseFS(fs,"contact.gohtml","tailwind.gohtml"))))
+		views.Must(views.ParseFS(fs, "contact.gohtml", "tailwind.gohtml"))))
 
 	// tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
 	// if err != nil {
 	// 	panic(err)
 	// }
 	r.Get("/faq", controllers.FAQ(
-		views.Must(views.ParseFS(fs, "faq.gohtml","tailwind.gohtml"))))
-	
-	r.Get("/signup", controllers.FAQ(
-		views.Must(views.ParseFS(fs, "signup.gohtml","tailwind.gohtml"))))
+		views.Must(views.ParseFS(fs, "faq.gohtml", "tailwind.gohtml"))))
+
+	usersC := controllers.Users{}
+	usersC.Templates.New = views.Must(views.ParseFS(fs, "signup.gohtml", "tailwind.gohtml"))
+	r.Get("/signup",usersC.New)
+	// r.Get("/signup", controllers.FAQ(
+	// 	views.Must(views.ParseFS(fs, "signup.gohtml", "tailwind.gohtml"))))
 	r.Get("/contact/{user-id}", MyRequestHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "page not found", http.StatusNotFound)
