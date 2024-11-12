ac787a9ebe13f4dd5804953ef4a5c00f8e3a062e
Himanshu <mehra.himanshu1708@gmail.com>
2024-11-12 13:11:02 +0530

    execting sql with go and inserting record with go

--- cmd/main.go
+++ cmd/main.go
@@ -44,4 +44,36 @@ func main() {
 		panic(err)
 	}
 	fmt.Println("connected")
+
+	//create a table
+	_,err = db.Exec(`
+	create table if not exists users(
+	id serial primary key,
+	name text , 
+	email text unique not null
+	);
+
+	create table if not exists orders(
+	id serial primary key,
+	user_id int not null,
+	amount int ,
+	description text
+	);
+	`)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("tables created")
+
+	// insert some data 
+	name := "himanshumehara"
+	email := "hm@meh.io"
+	_,err = db.Exec(`
+	insert into users(name,email)
+	values($1,$2);
+	`,name,email)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("user record inserted")
 }
