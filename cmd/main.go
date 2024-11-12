ba623c08035b9ed92408fbf02acfcdbe34a05c59
Himanshu <mehra.himanshu1708@gmail.com>
2024-11-12 16:03:28 +0530

    performing database operations

--- cmd/main.go
+++ cmd/main.go
@@ -66,14 +66,54 @@ func main() {
 	fmt.Println("tables created")
 
 	// insert some data 
-	name := "himanshumehara"
-	email := "hm@meh.io"
-	_,err = db.Exec(`
-	insert into users(name,email)
-	values($1,$2);
-	`,name,email)
-	if err != nil {
-		panic(err)
+	// name := "new"
+	// email := "newm@meh.io"
+
+	// // query := fmt.Sprintf(`
+	// // 	insert into users(name,email)
+	// // 	values ('%s','%s');
+	// // `,name,email)
+	// // fmt.Printf("executing: %s\n",query)
+	// // _,err = db.Exec(query)
+
+	// row := db.QueryRow(`
+	// insert into users(name,email)
+	// values($1,$2)returning id;
+	// `,name,email)
+	// row.Err() 
+	// var id int 
+	// err  = row.Scan(&id)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("user record inserted id ",id)
+
+	// var name string 
+	// var email string 
+	// id := 1
+	// row := db.QueryRow(`
+	// select name,email
+	// from users
+	// where id=$1;`,id)
+	// err = row.Scan(&name,&email)
+	// if err == sql.ErrNoRows {
+	// 	fmt.Println("error ,no rows!")
+	// }
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Printf("user info: name=%s,email=%s\n",name,email)
+
+	userID := 1 
+	for i := 1; i <= 5; i++ {
+		amount := i * 100 
+		desc := fmt.Sprintf("fake order #%d",i)
+		_,err := db.Exec(`
+		insert into orders(user_id,amount,description)
+		values($1,$2,$3)`,userID,amount,desc)
+		if err != nil {
+			panic(err)
+		}
 	}
-	fmt.Println("user record inserted")
+	fmt.Println("created fale orders. ")
 }
