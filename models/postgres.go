c8e812fdce13955d53dd39f22b0a50e73cec9d9e
Himanshu <mehra.himanshu1708@gmail.com>
2024-11-23 17:33:03 +0530

    running goose with go

--- models/postgres.go
+++ models/postgres.go
@@ -3,15 +3,17 @@ package models
 import (
 	"database/sql"
 	"fmt"
+
 	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/pressly/goose/v3"
 )
 
 func Open(config PostgresConfig) (*sql.DB, error) {
 	db, err := sql.Open("pgx", config.String())
 	if err != nil {
-		return nil, fmt.Errorf("open %w",err)
+		return nil, fmt.Errorf("open %w", err)
 	}
-	return db,nil
+	return db, nil
 }
 
 func DefaultPostgresConfig() PostgresConfig {
@@ -37,4 +39,16 @@ type PostgresConfig struct {
 func (cfg PostgresConfig) String() string {
 	return fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.SSLMode)
 
+}
+
+func Migrate(db *sql.DB, dir string) error {
+	err := goose.SetDialect("postgres")
+	if err != nil {
+		return fmt.Errorf("migrate: %w", err)
+	}
+	err = goose.Up(db,dir)
+	if err != nil {
+		return fmt.Errorf("migrate: %w",err)
+	}
+	return nil
 }
