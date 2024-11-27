007d8587668e210d31d9589add5034f990ab1ab0
Himanshu <mehra.himanshu1708@gmail.com>
2024-11-27 17:22:28 +0530

    initializing sevices with env vars

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 	"webdev/controllers"
 	"webdev/migrations"
 	"webdev/models"
@@ -11,17 +12,8 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/gorilla/csrf"
+	"github.com/joho/godotenv"
 )
-
-// func executeTemplate(w http.ResponseWriter, filepath string) {
-// 	t, err := views.Parse((filepath))
-// 	if err != nil {
-// 		log.Printf("parsing template: %v", err)
-// 		http.Error(w, "there was an error parsing the template.", http.StatusInternalServerError)
-// 		return
-// 	}
-// 	t.Execute(w, nil)
-// }
 
 func MyRequestHandler(w http.ResponseWriter, r *http.Request) {
 	// fetch the url parameter `"userID"` from the request of a matching
@@ -34,10 +26,50 @@ func MyRequestHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(fmt.Sprintf("hi %v, %v", userID, key)))
 }
 
+type config struct {
+	PSQL models.PostgresConfig
+	SMTP models.SMTPConfig
+	CSRF struct {
+		Key    string
+		Secure bool
+	}
+	Server struct {
+		Address string
+	}
+}
+
+func loadEnvConfig() (config, error) {
+	var cfg config
+	err := godotenv.Load()
+	if err != nil {
+		return cfg, err
+	}
+	cfg.PSQL = models.DefaultPostgresConfig()
+	cfg.SMTP.Host = os.Getenv("SMTP_HOST")
+	portStr := os.Getenv("SMTP_PORT")
+	cfg.SMTP.Port , err = strconv.Atoi(portStr)
+	if err != nil {
+		return cfg,err
+	}
+	// cfg.SMTP.Port = os.Getenv("SMTP_PORT")
+	cfg.SMTP.Username = os.Getenv("SMTP_USERNAME")
+	cfg.SMTP.Password = os.Getenv("SMTP_PASSWORD")
+
+	cfg.CSRF.Key = "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
+	cfg.CSRF.Secure = false
+	cfg.Server.Address = ":3000"
+
+	return cfg, nil
+}
+
 func main() {
-	cfg := models.DefaultPostgresConfig()
-	fmt.Println(cfg.String())
-	db, err := models.Open(cfg)
+	cfg,err := loadEnvConfig()
+	if err != nil {
+		panic(err)
+	}
+	// cfg := models.DefaultPostgresConfig()
+	// fmt.Println(cfg.String())
+	db, err := models.Open(cfg.PSQL)
 	if err != nil {
 		panic(err)
 	}
@@ -48,104 +80,66 @@ func main() {
 		panic(err)
 	}
 
-	userService := models.UserService{
+	userService := &models.UserService{
 		DB: db,
 	}
-	sessionService := models.SessionService{
+	sessionService := &models.SessionService{
 		DB: db,
 	}
+	pwResetService := &models.PasswordResetService{
+		DB: db,
+	}
+	emailService := models.NewEmaiService(cfg.SMTP)
 
 	umw := controllers.UserMiddlefware{
-		SessionService: &sessionService,
+		SessionService: sessionService,
 	}
 
 	fs := os.DirFS("templates")
-	csrfKey := "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
-	csrfMw := csrf.Protect([]byte(csrfKey), csrf.Secure(false),)
+	// csrfKey := "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
+	csrfMw := csrf.Protect([]byte(cfg.CSRF.Key), csrf.Secure(cfg.CSRF.Secure))
 
 	usersC := controllers.Users{
-		UserService:    &userService,
-		SessionService: &sessionService,
+		UserService:   userService,
+		SessionService: sessionService,
+		PasswordResetService: pwResetService,
+		EmailService: emailService,
 	}
 	usersC.Templates.New = views.Must(views.ParseFS(fs, "signup.gohtml", "tailwind.gohtml"))
 	usersC.Templates.SignIn = views.Must(views.ParseFS(fs, "signin.gohtml", "tailwind.gohtml"))
 	usersC.Templates.ForgotPassword = views.Must(views.ParseFS(fs, "forgot-pw.gohtml", "tailwind.gohtml"))
 
-
 	r := chi.NewRouter()
 	r.Use(csrfMw)
 	r.Use(umw.SetUser)
-	// tpl, err := views.Parse(filepath.Join("templates", "home.gohtml"))
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fs := os.DirFS("templates")
 	r.Get("/", controllers.StaticHandler(
 		views.Must(views.ParseFS(fs, "home.gohtml", "tailwind.gohtml"))))
-	// tpl, err = views.Parse(filepath.Join("templates", "contact.gohtml"))
-	// if err != nil {
-	// 	panic(err)
-	// }
 	r.Get("/contact", controllers.StaticHandler(
 		views.Must(views.ParseFS(fs, "contact.gohtml", "tailwind.gohtml"))))
-	// tpl, err = views.Parse(filepath.Join("templates", "faq.gohtml"))
-	// if err != nil {
-	// 	panic(err)
-	// }
 	r.Get("/faq", controllers.FAQ(
 		views.Must(views.ParseFS(fs, "faq.gohtml", "tailwind.gohtml"))))
-	// cfg := models.DefaultPostgresConfig()
-	// fmt.Println(cfg.String())
-	// db, err := models.Open(cfg)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer db.Close()
 
-	// err = models.MigrateFS(db, migrations.FS, ".")
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// userService := models.UserService{
-	// 	DB: db,
-	// }
-	// sessionService := models.SessionService{
-	// 	DB: db,
-	// }
-	// usersC := controllers.Users{
-	// 	UserService:    &userService,
-	// 	SessionService: &sessionService,
-	// }
-	// usersC.Templates.New = views.Must(views.ParseFS(fs, "signup.gohtml", "tailwind.gohtml"))
-	// usersC.Templates.SignIn = views.Must(views.ParseFS(fs, "signin.gohtml", "tailwind.gohtml"))
 	r.Get("/signup", usersC.New)
 	r.Post("/users", usersC.Create)
 	r.Get("/signin", usersC.SignIn)
 	r.Post("/signin", usersC.ProcessSignIn)
-	r.Get("/forgot-pw",usersC.ForgotPassword)
-	r.Post("/forgot-pw",usersC.ProcessForgotPassword)
-	r.Route("/users/me",func(r chi.Router) {
+	r.Get("/forgot-pw", usersC.ForgotPassword)
+	r.Post("/forgot-pw", usersC.ProcessForgotPassword)
+	r.Route("/users/me", func(r chi.Router) {
 		r.Use(umw.RequireUser)
-		r.Get("/",usersC.CurrentUser)
+		r.Get("/", usersC.CurrentUser)
 	})
 	// r.Get("/users/me", usersC.CurrentUser)
 	r.Post("/signout", usersC.ProcessSignOut)
-	// r.Get("/signup", controllers.FAQ(
-	// 	views.Must(views.ParseFS(fs, "signup.gohtml", "tailwind.gohtml"))))
 	r.Get("/contact/{user-id}", MyRequestHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "page not found", http.StatusNotFound)
 	})
-	// fmt.Println("starting the server on :3000...")
-	// umw := controllers.UserMiddlefware{
-	// 	SessionService: &sessionService,
-	// }
-
-	// csrfKey := "gFvi45R4fy5xNBlnEeZtQbfAVCYEIAUX"
-	// csrfMw := csrf.Protect([]byte(csrfKey), csrf.Secure(false))
-	fmt.Println("starting the server on :3000...")
-	http.ListenAndServe(":3000", r)
+	fmt.Printf("starting the server on %s.../n",cfg.Server.Address)
+	err = http.ListenAndServe(cfg.Server.Address, r)
+	if err != nil {
+		panic(err)
+	}
 }
 
 // func TimerMiddleWare(h http.HandlerFunc) http.HandlerFunc {
