bd39cfb16fbc22442d796f2a00bf66c5df6590a6
Himanshu <mehra.himanshu1708@gmail.com>
2024-11-28 13:51:24 +0530

    implementing consume function

--- models/password_reset.go
+++ models/password_reset.go
@@ -75,11 +75,50 @@ func (service *PasswordResetService) Create(email string) (*PasswordReset, error
 }
 
 func (service *PasswordResetService) Consume(token, newPassword string) (*User, error) {
-	return nil, fmt.Errorf("implement password reset service ")
+	tokenHash := service.hash(token)
+	var user User 
+	var pwReset PasswordReset 
+	row := service.DB.QueryRow(`
+	select password_resets.id,
+	password_resets.expires_at,
+	users.id,
+	users.email,
+	users.password_hash
+	from password_resets
+	join users on users.id = password_resets.user_id
+	where password_resets.token_hash=$1;`,tokenHash)
+	err := row.Scan(&pwReset.ID,&pwReset.ExpiresAt,&user.ID, &user.Email,
+	&user.PasswordHash)
+	if err != nil {
+		return nil, fmt.Errorf("consume: %w",err)
+
+	}
+	if time.Now().After(pwReset.ExpiresAt) {
+		return nil, fmt.Errorf("token expires : %w",token)
+	}
+
+	err = service.delete(pwReset.ID) 
+	if err != nil {
+		return nil,fmt.Errorf("consume: %w",err)
+	}
+	return &user,nil
+	// return nil, fmt.Errorf("implement password reset service ")
+
 }
 
 func (service *PasswordResetService) hash(token string) string {
 	tokenHash := sha256.Sum256([]byte(token))
 	return base64.URLEncoding.EncodeToString(tokenHash[:])
 
+}
+
+
+func (service *PasswordResetService) delete(id int) error {
+	_,err := service.DB.Exec(`
+	delete from password_resets
+	where id = $1;`,id)
+	if err != nil {
+		return fmt.Errorf("delete: %w",err)
+	}
+	return nil 
 }
