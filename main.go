254caec541d155a481cc23096957c0beb5670e18
Himanshu <mehra.himanshu1708@gmail.com>
2024-12-02 19:13:21 +0530

    created a handler to delete image

--- main.go
+++ main.go
@@ -164,6 +164,7 @@ func main() {
 			r.Post("/{id}", galleriesC.Update)
 			r.Post("/{id}/delete", galleriesC.Delete)
 			r.Get("/",galleriesC.Index)
+			r.Post("/{id}/images/{filename}/delete", galleriesC.DeleteImage)
 		})
 	})
 
